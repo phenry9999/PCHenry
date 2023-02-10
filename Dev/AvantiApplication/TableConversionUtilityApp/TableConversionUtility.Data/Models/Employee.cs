3434a02ae234e1c18efd031fda6aae529bcd3c13
PHenry <phenry_w@hotmail.com>
2023-02-10 17:10:57 -0500

    Adding resources cause I can, I plan to add them later if I have time. Adding Employee shell and thinking about the main form layout.

---
+++ Dev/AvantiApplication/TableConversionUtilityApp/TableConversionUtility.Data/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableConversionUtility.Data.Models
+{
+	public class Employee
+	{
+		public string FirstName { get; set; }
+		public string Department { get; set; }
+		public int Age { get; set; }
+	}
+}
