b1470c33cf49089bb99d7b4152a719f815d78b11
PHenry <phenry_w@hotmail.com>
2023-02-12 15:59:07 -0500

    Adding tests and some other things I think I'll need

---
+++ Dev/AvantiApplication/TableConversionUtility/TableConversionUtility.Data/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableConversionUtility.Data.Models
+{
+	public class Employee
+	{
+		[Required(ErrorMessage = "This is the PK and is required.")]
+		[DisplayName("First Name")]
+		public string FirstName { get; set; }
+
+		[StringLength(100, ErrorMessage = "Department name needs to be less than 100 characters to be valid.")]
+		public string? Department { get; set; }
+
+		[Range(1, 99, ErrorMessage = "Age range is 1 to 99 years old.")]
+		public int Age { get; set; }
+
+		public Employee()
+		{
+			this.FirstName = "DEFAULT";
+		}
+	}
+}
--- Dev/AvantiApplication/TableConversionUtility/TableConversionUtility.Tests/EmployeeTests.cs
+++ Dev/AvantiApplication/TableConversionUtility/TableConversionUtility.Tests/EmployeeTests.cs
@@ -4,10 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using TableConversionUtility.Data.Models;
-using TableConversionUtility.Models;
 
 namespace TableConversionUtility.Tests
 {
@@ -20,7 +17,7 @@ namespace TableConversionUtility.Tests
 			Employee employee = new Employee();
 
 			Assert.IsNotNull(employee);
-			Assert.IsNull(employee.FirstName);
+			Assert.IsNotNull(employee.FirstName);
 			Assert.IsNull(employee.Department);
 			Assert.AreEqual(employee.Age, 0);
 		}
@@ -34,30 +31,18 @@ namespace TableConversionUtility.Tests
 			Assert.AreEqual(employee.Department, "Development");
 			Assert.AreEqual(employee.Age, 27);
 		}
-	}
 
-	[TestClass]
-	public class Employee2Tests
-	{
 		[TestMethod]
-		public void DefaultEmployeeCreate()
+		public void CreateEmptyEmployeeThenDefaultNameIsCorrect()
 		{
-			Employee2 employee = new Employee2();
+			Employee employee = new Employee();
 
 			Assert.IsNotNull(employee);
-			Assert.IsNull(employee.FirstName);
-			Assert.IsNull(employee.Department);
-			Assert.AreEqual(employee.Age, 0);
-		}
-
-		[TestMethod]
-		public void CreateSpecificEmployeeIsCorrectlyRead()
-		{
-			Employee2 employee = new Employee2 { FirstName = "Martin", Department = "Development", Age = 27 };
-
-			Assert.AreEqual(employee.FirstName, "Martin");
-			Assert.AreEqual(employee.Department, "Development");
-			Assert.AreEqual(employee.Age, 27);
+			Assert.IsNotNull(employee.FirstName);
+			Assert.AreEqual(employee.FirstName, "DEFAULT");
+			Assert.AreNotEqual(employee.FirstName, "Default");
+			Assert.AreNotEqual(employee.FirstName, "default");
 		}
 	}
 }
+
