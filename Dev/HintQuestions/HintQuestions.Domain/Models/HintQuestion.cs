edbaf33d0668058206eab4830ea14fb320214449
PHenry <phenry_w@hotmail.com>
2025-11-04 15:38:34 -0500

    old stuff

---
+++ Dev/HintQuestions/HintQuestions.Domain/Models/HintQuestion.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HintQuestions.Domain.Models;
+
+public class HintQuestion
+{
+	[Key]
+	[Required]
+	public int Id { get; set; }
+
+	[Required(ErrorMessage = "Question is a required field")]
+	public string Question { get; set; }
+
+	public List<HintAnswer> HintAnswers { get; set; }
+}
+
+public class HintAnswer
+{
+	[Key]
+	[Required]
+	public int Id { get; set; }
+
+	[Required(ErrorMessage = "HintQuestionId is a required field")]
+	public int HintQuestionId { get; set; }
+
+	[Required(ErrorMessage = "Answer is a required field")]
+	public string Answer { get; set; }
+
+	public HintQuestion HintQuestion { get; set; }
+}
