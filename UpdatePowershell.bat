1957fa32c1cdc0be37329f0166613d6326bccb69
Peter Henry <PHenry@AvantiSystems.com>
2026-04-23 11:52:07 -0400

    Updated to make work

--- UpdatePowershell.bat
+++ UpdatePowershell.bat
@@ -1,1 +1,4 @@
-winget upgrade --id Microsoft.PowerShell
+rem winget install --id Microsoft.PowerShell
+rem winget upgrade --id Microsoft.PowerShell
+
+iex "& { $(irm https://aka.ms/install-powershell.ps1) }"
