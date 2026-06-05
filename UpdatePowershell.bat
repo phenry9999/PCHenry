bfecd17a3a9eb72b6beaab59aaaf0913485ceb49
PHenry <phenry_w@hotmail.com>
2026-06-05 14:48:38 -0400

    modified update bat and delting old zeerka backups

--- UpdatePowershell.bat
+++ UpdatePowershell.bat
@@ -1,4 +1,1 @@
-rem winget install --id Microsoft.PowerShell
-rem winget upgrade --id Microsoft.PowerShell
-
-iex "& { $(irm https://aka.ms/install-powershell.ps1) }"
+winget upgrade --id Microsoft.PowerShell
